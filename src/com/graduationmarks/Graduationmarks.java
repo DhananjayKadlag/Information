9d648a532ef4efe69c4925f6127c5c2ab4a4be27
DhananjayKadlag <dckadlag22@gmail.com>
2022-02-21 15:30:49 +0530

    "hi graduation details"

---
+++ src/com/graduationmarks/Graduationmarks.java
@@ -0,0 +1,11 @@
+package com.graduationmarks;
+// Graduation Marks 
+
+public class Graduationmarks 
+{
+public static void main(String[] args)
+{
+	System.out.println("B.E. Mechanical-69% and M.E.Design-8.4 SGPA");
+	
+}
+}
